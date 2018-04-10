d494959e6fd9257d86ea92209220cc6f8f82e6a8
Toby HUANG <619058900@qq.com>
2018-04-10 16:36:50 +1000

    Try to write the legal C and toy programme

--- asst1/C_code/uniq.c
+++ asst1/C_code/uniq.c
@@ -1,53 +1,55 @@
-int length_str(char *a){
-    int i = 0;
-    while(a[i]!='\0'){
-        i = i+1;
-    }
-    return i;
-}
-
-void copy_str(char *a, char *b){
-    // copy a particular string from a to b 
-    int i= 0;
-    while(i <= length_str(a) ){
-        b[i] = a[i];
-        i++;
-    }
-}
-
-int compare_str(char *a, char *b){
-    int  flag = 1;
-    if(length_str(a) == length_str(b))
-    {
-        int i =0;
-        while (i <= length_str(a)){
-            if (a[i]!= b[i]){
-                flag = 0;
-            }
-            else{
-                // skip
-            }
-            i++;
-        }
-    }
-    else{
-        flag =0;        
-    }
-    return flag;
-}
-
 unsigned int uniq(unsigned int n, char *a[], char *b[]){
-    int i,j;
+    int i,j,k;
+    // store the length of String
+    int aLength, bLength,flag;
     if(n > 0){
-        copy_str(a[0],b[0]);
+        aLength= 0;
+        
+        while(a[0][aLength]!='\0'){
+            aLength = aLength+1;
+        }
+        
+        k= 0;
+        while(k <= aLength ){
+                b[0][k] = a[0][k];
+                k= k+1;
+        }
         i =j =1;
     }
     else{
         i = j=0;
     }
     while(i<n){
-        if (!compare_str(a[i], a[i-1])){
-            copy_str(a[i],b[j]);
+        aLength =bLength = 0;
+        while(a[i][aLength]!='\0'){
+            aLength = aLength+1;
+        }
+        while(a[i-1][bLength]!='\0'){
+            bLength = bLength+1;
+        }
+        flag = 1;
+        if(aLength == bLength)
+        {
+            k =0;
+            while (k <= aLength){
+                if (a[i][k]!= a[i-1][k]){
+                    flag = 0;
+                }
+                else{
+                    // skip
+                }
+                k= k+1;
+            }
+        }
+        else{
+            flag =0;
+        }
+        if (!flag){
+            k= 0;
+            while(k <= aLength){
+                b[j][k] = a[i][k];
+                k= k+1;
+            }
             j++;
         }
         else{
